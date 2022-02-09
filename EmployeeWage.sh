55f20b12f76e995033bfcdfc106b7caeba6a4f48
Ashutosh Pandey <baralbisnupriya7@gmail.com>
2022-02-09 08:18:13 +0530

    Added salary

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,9 +1,14 @@
 isPresent=1
+rateprhr=20
 check=$((RANDOM%2))
 
 if [ $check -eq 1 ]
  then
     echo "Employee is present"
+    wrkhrs=8
  else
     echo "Emplyee is Absent"
+    wrkhrs=0
 fi
+salary=$(($rateprhr*$wrkhrs))
+echo "Salary is $salary"
